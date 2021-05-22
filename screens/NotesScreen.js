a62fc96025765590514e88556ca61c5ef84f5339
elladiablo <elladiablo@gmail.com>
2021-05-22 15:43:45 +0800

    It works

--- screens/NotesScreen.js
+++ screens/NotesScreen.js
@@ -32,15 +32,27 @@ export default function NotesScreen({ navigation, route }) {
     //return unsubscribe; //return the cleanup function
   //}, []);
 
-//const newNote = {
-  //title: route.params.text,
-  //done:false, //no more id line!
-  //created: firebase.firestore.FieldValue.serverTimestamp(),
-  //alternative
-  //created: Date.now().toString(),
-//};
+// const newNote = {
+//   title: route.params.text,
+//   done:false, //no more id line!
+//   created: firebase.firestore.FieldValue.serverTimestamp(),
+//   //alternative
+//   created: Date.now().toString(),
+// };
 //db.add(newNote);
 //}
+
+// Monitor route.params for changes and add items to the database
+useEffect(() => {
+  if (route.params?.text) {
+    const newNote = {
+      title: route.params.text,
+      done: false,
+      created: firebase.firestore.FieldValue.serverTimestamp(),
+    };
+    db.add(newNote);
+  }
+}, [route.params?.text]);
 
   //firebase.firestore().collection("testing").add({
     //title: "Testing! Does this work??",
@@ -54,7 +66,14 @@ export default function NotesScreen({ navigation, route }) {
     .firestore()
     .collection("todos")
     .onSnapshot((collection)=> { // Let's get back a snapshot of this collection
-      const updatedNotes = collection.docs.map((doc) => doc.data());
+      const updatedNotes = collection.docs.map((doc) => {
+        const noteObject ={
+         ...doc.data(),
+          id: doc.id,
+        };
+        console.log(noteObject);
+        return noteObject;
+      });
       setNotes(updatedNotes); //And set our notes state array to its docs
   });
 
@@ -63,6 +82,24 @@ export default function NotesScreen({ navigation, route }) {
     unsubscribe ();
   };
 },[]);
+
+useEffect(() => {
+  navigation.setOptions({
+    headerRight: () => (
+      <TouchableOpacity onPress={addNote}>
+        <Ionicons
+          name="ios-create-outline"
+          size={30}
+          color="black"
+          style={{
+            color: "#f55",
+            marginRight: 10,
+          }}
+        />
+      </TouchableOpacity>
+    ),
+  });
+});
 
   function addNote() { 
     navigation.navigate("Add Screen");
@@ -102,6 +139,7 @@ export default function NotesScreen({ navigation, route }) {
         }}
       >
         <Text>{item.title}</Text>
+        <Text>{item.created?.toDate().toDateString()}</Text>
         <TouchableOpacity onPress={() => deleteNote(item.id)}>
           <Ionicons name="trash" size={16} color="#944" />
         </TouchableOpacity>
